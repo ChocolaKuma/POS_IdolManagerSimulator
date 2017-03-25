7e41b08933fb9385c9c9cc4c0e5d61d1bdcfa980
Johnathan Hinebrook <johnisadept@live.com>
2017-03-24 20:44:46 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# POS_IdolManagerSimulator
-Piece of Sh!t Idol Manager Simulator. I was enspired by some many idol sims
+# IdolManagerSimulator
+A stupidly basic Idol Manager game that is console based<br>
+Inspired by:<br>
+https://lemmasoft.renai.us/forums/viewtopic.php?f=60&t=29331&p=347603&hilit=idol#p347603 <br>
+https://lemmasoft.renai.us/forums/viewtopic.php?f=43&t=37865&hilit=idol <br>
+
+<br>
+The only dependencies are random and python3x 
+<br>
+I might build this out one day
+
+<br>
+<br>
+TODO:<br>
+Add gui<br>
+Add idol pictures<br>
+Add measurments <br>
+Add Blood Types<br>
+Add random Events<br>
---
+++ idlemanager.py
@@ -0,0 +1,48 @@
+import random
+
+firstName = ["Haruto","Riku","Haru","Hinata","Sora"]
+lastName = ["Saito","Kobayashi","Nakamura","Yamamoto","Ito"]
+
+def trainingDay(talent):
+    print("\nTime For Some Training\n")
+    talent = talent + random.randint(1,4)
+    return talent
+    
+
+
+    
+class girl(object):
+    def NameMaker(name):
+        pick = random.randint(0,4)  
+        r = name[pick]
+        return r
+    def bdaygen():
+        month = random.randint(1,12)
+        day = random.randint(1,12)
+        r = str(month)+"/"+str(day)
+        return r
+    def __init__(self):
+        self.fName = girl.NameMaker(firstName)
+        self.lName = girl.NameMaker(lastName)
+        self.age = random.randint(14,22)
+        self.bday = girl.bdaygen() 
+        self.talent = random.randint(1,5)
+        self.energy = random.randint(5,20)
+        self.cuteness = random.randint(1,10)
+        self.signed = False
+
+bla = girl()
+  
+    select = int(input("\nOption number:"))
+    if(select == 0):
+        print("Looks like your dreams of being an\nIdol manager are over eh?")
+        keepGoing = False
+    if(select == 1):
+        bla.energy = bla.energy + random.randint(1,2)    
+    if(select == 2):
+        if(bla.energy>0):
+            bla.energy = bla.energy - 1
+            bla.talent = trainingDay(bla.talent)
+        if(bla.energy<0):
+            print("You dont have enough energy")
+
