eb74d753f919150ea03879e86667b101c6d0c152
Johnathan Hinebrook <johnisadept@live.com>
2017-03-31 22:44:15 -0400

    Add files via upload

--- idlemanager.py
+++ idlemanager.py
@@ -2,27 +2,54 @@ import random
 
 firstName = ["Haruto","Riku","Haru","Hinata","Sora"]
 lastName = ["Saito","Kobayashi","Nakamura","Yamamoto","Ito"]
+minValue = 1
+MaxTalent = 5
+MaxEnergy = 20
+MaxCuteness = 10
+CurrentDay = 1
+CashOnHand = 9999999
+CashSign = "¥"
 
-def old_options():    
-    select = int(input("\nOption number:"))
-    if(select == 0):
-        print("Looks like your dreams of being an\nIdol manager are over eh?")
-        keepGoing = False
-    if(select == 1):
-        bla.energy = bla.energy + random.randint(1,2)    
-    if(select == 2):
-        if(bla.energy>0):
-            bla.energy = bla.energy - 1
-            bla.talent = trainingDay(bla.talent)
-        if(bla.energy<0):
-            print("You dont have enough energy")
-
-
-def trainingDay(talent):
-    print("\nTime For Some Training\n")
-    talent = talent + random.randint(1,4)
-    return talent
-    
+class actions():
+    def TalentTraining(girl):
+        global CashOnHand 
+        CashOnHand = CashOnHand - 500
+        if(girl.talent < MaxTalent):
+            t = random.randint(1,4)
+            Pt = girl.talent + t
+            if(Pt >= MaxTalent):
+                girl.talent = MaxTalent
+            if(Pt < MaxTalent):
+                girl.talent = girl.talent + t
+    def EnergyTraining(girl):
+        global CashOnHand 
+        CashOnHand = CashOnHand - 500
+        if(girl.energy < MaxEnergy):
+            t = random.randint(1,20)
+            Pt = girl.energy + t      
+            if(Pt >= MaxEnergy):
+                girl.energy = MaxEnergy
+            if(Pt < MaxEnergy):
+                girl.energy = girl.energy + t
+                if(girl.energy > MaxEnergy):
+                    girl.energy = MaxEnergy 
+    def CuteTraining(girl):
+        global CashOnHand 
+        CashOnHand = CashOnHand - 500
+        if(girl.cuteness < MaxCuteness):
+            t = random.randint(1,MaxCuteness/2)
+            Pt = girl.energy + t      
+            if(Pt >= MaxCuteness):
+                girl.cuteness = MaxCuteness
+            if(Pt < MaxCuteness):
+                girl.cuteness = girl.cuteness + t
+                if(girl.cuteness > MaxCuteness):
+                    girl.cuteness = MaxCuteness                    
+    def PopularityTraining(girl):
+        global CashOnHand 
+        CashOnHand = CashOnHand - 500
+        girl.energy = girl.energy - random.randint(5,10)
+        girl.popularity + random.randint(5,500)  
 class girl(object):
     def NameMaker(name):
         pick = random.randint(0,4)  
@@ -41,7 +68,17 @@ class girl(object):
         self.talent = random.randint(1,5)
         self.energy = random.randint(5,20)
         self.cuteness = random.randint(1,10)
+        self.popularity = 0
         self.signed = False
 
+def test():
+    global CashOnHand
+    global CurrentDay
+    g = girl()
+    print(CashSign,CashOnHand)
+    print("Energy:"+str(g.cuteness)+"/"+str(MaxCuteness))
 
-  
+    while g.cuteness < MaxCuteness:  
+        actions.CuteTraining(g)
+        print(CashSign,CashOnHand)
+        print("Energy:"+str(g.cuteness)+"/"+str(MaxCuteness)) 
--- posfrontend.py
+++ posfrontend.py
@@ -1,18 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
+import idlemanager as im
 
-day = 1
+CurrentDay = 1
+CashOnHand = 9999999
+
 LARGE_FONT= ("Verdana", 12)
 starttext = """This Game is still in development
 Please keep that in mind.
 Nothing in this is the final product."""
-def nextday():
-    global day
-    day = day + 1
-    print(day)
-
+Idol = im.girl()
 class POSIS(tk.Tk):
-
     def __init__(self, *args, **kwargs):
         
         tk.Tk.__init__(self, *args, **kwargs)
@@ -25,7 +23,7 @@ class POSIS(tk.Tk):
 
         self.frames = {}
 
-        for F in (StartPage, News, Girls,Staff,Infu,Events):
+        for F in (StartPage, News):
 
             frame = F(container, self)
 
@@ -39,7 +37,19 @@ class POSIS(tk.Tk):
 
         frame = self.frames[cont]
         frame.tkraise()
+def nextday():
+    global CurrentDay
+    CurrentDay = CurrentDay + 1
+    print(CurrentDay)
 
+def tT():
+    pass
+def eT():
+    pass
+def cT():
+    pass
+def pT():
+    pass
         
 class StartPage(tk.Frame):
 
@@ -52,138 +62,60 @@ class StartPage(tk.Frame):
                             command=lambda: controller.show_frame(News))
         button.grid(row=2,column=1)
         
-        button = ttk.Button(self, text="Load Game\nNot Devloped",
-                            command=lambda: controller.show_frame(News))
-        button.grid(row=2,column=3)
+        #button = ttk.Button(self, text="Load Game\nNot Devloped",
+                            #command=lambda: controller.show_frame(News))
+        #button.grid(row=2,column=3)
 
 
 class News(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        button1 = ttk.Button(self, text="News",
+        button1 = ttk.Button(self, text="Talent Training",
                             command=lambda: controller.show_frame(News))
         button1.grid(row=2,column=1)
-        button2 = ttk.Button(self, text="Girls",
+        button2 = ttk.Button(self, text="Energy Training",
                             command=lambda: controller.show_frame(Girls))
         button2.grid(row=2,column=2)
-        button3 = ttk.Button(self, text="Staff",
+        button3 = ttk.Button(self, text="Cuteness Training",
                             command=lambda: controller.show_frame(Staff))
         button3.grid(row=2,column=3)
         button4 = ttk.Button(self, text="Infustucture",
                             command=lambda: controller.show_frame(Infu))
         button4.grid(row=2,column=4)
-        button4 = ttk.Button(self, text="Events",
+        button4 = ttk.Button(self, text="Popularity Training",
                             command=lambda: controller.show_frame(Events))
         button4.grid(row=2,column=5)   
-        currentselection = tk.Label(self, text="News", font=LARGE_FONT)
-        currentselection.grid(row=2,column=6)
-        daycount = tk.Label(self, text="Day"+str(day), font=LARGE_FONT)
+
+        daycount = tk.Label(self, text="Day"+str(CurrentDay), font=LARGE_FONT)
         daycount.grid(row=2,column=7)        
         button5 = ttk.Button(self, text="NextDay",
                             command=nextday)
-        button5.grid(row=2,column=8)         
-class Girls(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        button1 = ttk.Button(self, text="News",
-                            command=lambda: controller.show_frame(News))
-        button1.grid(row=2,column=1)
-        button2 = ttk.Button(self, text="Girls",
-                            command=lambda: controller.show_frame(Girls))
-        button2.grid(row=2,column=2)
-        button3 = ttk.Button(self, text="Staff",
-                            command=lambda: controller.show_frame(Staff))
-        button3.grid(row=2,column=3)
-        button4 = ttk.Button(self, text="Infustucture",
-                            command=lambda: controller.show_frame(Infu))
-        button4.grid(row=2,column=4)
-        button4 = ttk.Button(self, text="Events",
-                            command=lambda: controller.show_frame(Events))
-        button4.grid(row=2,column=5)   
-        currentselection = tk.Label(self, text="Girls", font=LARGE_FONT)
-        currentselection.grid(row=2,column=6)
-        button5 = ttk.Button(self, text="NextDay",
-                            command=nextday)
         button5.grid(row=2,column=8)
-        daycount = tk.Label(self, text="Day"+str(day), font=LARGE_FONT)
-        daycount.grid(row=2,column=7) 
-        ##TODO Add multigirl Support
-        
-
-class Staff(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        button1 = ttk.Button(self, text="News",
-                            command=lambda: controller.show_frame(News))
-        button1.grid(row=2,column=1)
-        button2 = ttk.Button(self, text="Girls",
-                            command=lambda: controller.show_frame(Girls))
-        button2.grid(row=2,column=2)
-        button3 = ttk.Button(self, text="Staff",
-                            command=lambda: controller.show_frame(Staff))
-        button3.grid(row=2,column=3)
-        button4 = ttk.Button(self, text="Infustucture",
-                            command=lambda: controller.show_frame(Infu))
-        button4.grid(row=2,column=4)
-        button4 = ttk.Button(self, text="Events",
-                            command=lambda: controller.show_frame(Events))
-        button4.grid(row=2,column=5)   
-        currentselection = tk.Label(self, text="Staff", font=LARGE_FONT)
-        currentselection.grid(row=2,column=6)
-        button5 = ttk.Button(self, text="NextDay",
-                            command=nextday)
-        button5.grid(row=2,column=8)
-        daycount = tk.Label(self, text="Day"+str(day), font=LARGE_FONT)
-        daycount.grid(row=2,column=7) 
-class Infu(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        button1 = ttk.Button(self, text="News",
-                            command=lambda: controller.show_frame(News))
-        button1.grid(row=2,column=1)
-        button2 = ttk.Button(self, text="Girls",
-                            command=lambda: controller.show_frame(Girls))
-        button2.grid(row=2,column=2)
-        button3 = ttk.Button(self, text="Staff",
-                            command=lambda: controller.show_frame(Staff))
-        button3.grid(row=2,column=3)
-        button4 = ttk.Button(self, text="Infustucture",
-                            command=lambda: controller.show_frame(Infu))
-        button4.grid(row=2,column=4)
-        button4 = ttk.Button(self, text="Events",
-                            command=lambda: controller.show_frame(Events))
-        button4.grid(row=2,column=5)   
-        currentselection = tk.Label(self, text="Infustucture", font=LARGE_FONT)
-        currentselection.grid(row=2,column=6)
-        button5 = ttk.Button(self, text="NextDay",
-                            command=nextday)
-        button5.grid(row=2,column=8)
-        daycount = tk.Label(self, text="Day"+str(day), font=LARGE_FONT)
-        daycount.grid(row=2,column=7) 
-class Events(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, parent)
-        button1 = ttk.Button(self, text="News",
-                            command=lambda: controller.show_frame(News))
-        button1.grid(row=2,column=1)
-        button2 = ttk.Button(self, text="Girls",
-                            command=lambda: controller.show_frame(Girls))
-        button2.grid(row=2,column=2)
-        button3 = ttk.Button(self, text="Staff",
-                            command=lambda: controller.show_frame(Staff))
-        button3.grid(row=2,column=3)
-        button4 = ttk.Button(self, text="Infustucture",
-                            command=lambda: controller.show_frame(Infu))
-        button4.grid(row=2,column=4)
-        button4 = ttk.Button(self, text="Events",
-                            command=lambda: controller.show_frame(Events))
-        button4.grid(row=2,column=5)   
-        currentselection = tk.Label(self, text="Events", font=LARGE_FONT)
-        currentselection.grid(row=2,column=6)
-        button5 = ttk.Button(self, text="NextDay",
-                            command=nextday)
-        button5.grid(row=2,column=8)
-        daycount = tk.Label(self, text="Day"+str(day), font=LARGE_FONT)
-        daycount.grid(row=2,column=7) 
+#########################################
+        #TODO Make this fucker work!!!
+        #img = tk.PhotoImage(file="img\girl.jpg")
+        #img = img.zoom(ZoomAMT) 
+        #img = img.subsample(40)
+        #logoPNG = Label(self, image=img).grid(row=3,column=1) 
+        #label = tk.Label(image="./img/girl.jpg")
+        #label.image = photo # keep a reference!
+        #label.grid(row=3,rowspan=,column=1)
+#########################################
+        NameLabel = tk.Label(self, text="Name:"+Idol.fName+" "+Idol.lName, font=LARGE_FONT)
+        NameLabel.grid(row=3,column=2) 
+        AgeLabel = tk.Label(self, text="Age:"+str(Idol.age)+" "+Idol.bday, font=LARGE_FONT)
+        AgeLabel.grid(row=4,column=2)         
+        TalentLabel = tk.Label(self, text="Talent:"+str(Idol.talent)+"/5", font=LARGE_FONT)
+        TalentLabel.grid(row=5,column=2) 
+        EnergyLabel = tk.Label(self, text="Energy:"+str(Idol.energy)+"/20", font=LARGE_FONT)
+        EnergyLabel.grid(row=6,column=2)
+        CuteLabel = tk.Label(self, text="Cuteness:"+str(Idol.cuteness)+" /10", font=LARGE_FONT)
+        CuteLabel.grid(row=7,column=2)
+        PopLabel = tk.Label(self, text="Popularity:"+str(Idol.popularity), font=LARGE_FONT)
+        PopLabel.grid(row=8,column=2)
+        SignedLabel = tk.Label(self, text="Signed:"+str(Idol.signed), font=LARGE_FONT)
+        SignedLabel.grid(row=9,column=2)
+        MoneyLabel = tk.Label(self, text="Money:"+"¥"+str(CashOnHand), font=LARGE_FONT)
+        MoneyLabel.grid(row=10,column=2)
 app = POSIS()
 app.mainloop()
